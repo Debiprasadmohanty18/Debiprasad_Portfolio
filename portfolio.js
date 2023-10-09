af93155a2a4e1ed96505d7528378b2359c409527
Tanmay <iamdebiprasad2001@gmail.com>
2023-10-10 00:16:05 +0530

    My Files

---
+++ portfolio.js
@@ -0,0 +1,9 @@
+
+    var typestyle = new Typed(".profession",{
+            strings : ["Student.","Frontend Developer.","Coder.","Gamer."],
+            loop : true,
+            typeSpeed : 100,
+            backSpeed : 80,
+            startDelay : 1000,
+            backDelay : 1500
+        })
